f527cab619ab6035a9f6863aec87fae92bba4713
Kevin Ruscoe <hello@kevinruscoe.me>
2019-04-19 13:38:00 +0100

    things

--- src/woocommerce-add-to-cart-redirect-fine-control.admin.php
+++ src/woocommerce-add-to-cart-redirect-fine-control.admin.php
@@ -7,20 +7,20 @@
  */
 
 /**
- * Update '_redirect_to_cart_action' meta key.
+ * Update '_after_add_to_cart_redirection_id' meta key.
  */
 add_action(
 	'woocommerce_process_product_meta',
 	function ( $post_id ) {
-		if ( isset( $_POST['woocommerce_meta_nonce'], $_POST['_redirect_to_cart_action'] ) ) {
+		if ( isset( $_POST['woocommerce_meta_nonce'], $_POST['_after_add_to_cart_redirection_id'] ) ) {
 			if ( ! wp_verify_nonce( sanitize_key( $_POST['woocommerce_meta_nonce'] ), 'woocommerce_save_data' ) ) {
 				exit;
 			}
 
 			$product = wc_get_product( $post_id );
 			$product->update_meta_data(
-				'_redirect_to_cart_action',
-				isset( $_POST['_redirect_to_cart_action'] ) ? 'yes' : 'no'
+				'_after_add_to_cart_redirection_id',
+				sanitize_text_field( wp_unslash( $_POST['_after_add_to_cart_redirection_id'] ) )
 			);
 			$product->save();
 		}
@@ -28,45 +28,27 @@ add_action(
 );
 
 /**
- * Adds a new tab named 'Sample Product' to the admin product data panel.
- */
-add_filter(
-	'woocommerce_product_data_tabs',
-	function ( $default_tabs ) {
-		$default_tabs['redirect_to_cart_action'] = array(
-			'label'    => 'Add To Cart Action',
-			'target'   => 'redirect_to_cart_action_tab_data',
-			'priority' => 100,
-		);
-		return $default_tabs;
-	}
-);
-
-/**
  * Displays content in the 'Sample Product' new tab.
  */
 add_action(
-	'woocommerce_product_data_panels',
+	'woocommerce_product_options_advanced',
 	function () {
-		?>
-		<div id="redirect_to_cart_action_tab_data" class="panel woocommerce_options_panel">
-			<?php
-			woocommerce_wp_select(
-				[
-					'id'                => '_add_to_cart_action',
-					'class'             => 'wc-product-search',
-					'label'             => 'Add To Cart Action',
-					'name'              => '_add_to_cart_action',
-					'style'             => 'width: 50%;',
-					'custom_attributes' => [
-						'data-placeholder' => 'Select action',
-						'data-action'      => 'add_to_cart_search_products_pages',
-					],
-				]
-			)
-			?>
-		</div>
-		<?php
+		woocommerce_wp_select(
+			[
+				'id'                => '_after_add_to_cart_redirection_id',
+				'class'             => 'wc-product-search',
+				'label'             => 'Redirect To',
+				'name'              => '_after_add_to_cart_redirection_id',
+				'style'             => 'width: 80%;',
+				'desc_tip'          => true,
+				'description'       => 'Where should you be redirected to after adding this product to the cart?',
+				'custom_attributes' => [
+					'data-placeholder' => 'Select URL',
+					'data-action'      => 'add_to_cart_fine_control_search_things',
+					'data-exclude'     => get_the_ID(),
+				],
+			]
+		);
 	}
 );
 
@@ -74,27 +56,41 @@ add_action(
  * Performs an AJAX admin call to find pages/products/posts.
  */
 add_action(
-	'wp_ajax_add_to_cart_search_products_pages',
+	'wp_ajax_add_to_cart_fine_control_search_things',
 	function() {
 		global $wpdb;
 
-		// @codingStandardsIgnoreLine
-		if ( ! isset( $_GET['term'] ) ) {
+		check_ajax_referer( 'search-products', 'security' );
+
+		if ( ! isset( $_GET['term'], $_GET['exclude'] ) ) {
 			wp_die();
 		}
 
-		// @codingStandardsIgnoreLine
-		$term = sanitize_text_field( wp_unslash( $_GET['term'] ) );
+		$term    = sanitize_text_field( wp_unslash( $_GET['term'] ) );
+		$exclude = absint( wp_unslash( $_GET['exclude'] ) );
 
 		// @codingStandardsIgnoreLine
 		$results = $wpdb->get_results(
 			$wpdb->prepare(
-				"select id, post_title, post_type from wp_posts where post_title like %s and post_status='publish' and post_type in ('post', 'page', 'product')",
-				'%' . $wpdb->esc_like( $term ) . '%'
+				'select id, post_title, post_type from wp_posts ' .
+				"where post_title like %s and post_status='publish' " .
+				"and post_type in ('post', 'page', 'product')" .
+				'and id != %d',
+				'%' . $wpdb->esc_like( $term ) . '%',
+				$exclude
 			)
 		);
 
-		$return = [];
+		$default = get_option( 'woocommerce_cart_redirect_after_add' );
+
+		$default_label = "Default Action (don't redirect to cart)";
+		if ( 'yes' === $default ) {
+			$default_label = 'Default Action (redirect to cart)';
+		}
+
+		$return = [
+			'NULL' => $default_label,
+		];
 
 		foreach ( $results as $result ) {
 			$return[ $result->id ] = sprintf(
