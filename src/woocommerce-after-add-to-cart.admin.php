95cb2e4f340263da597367b4b7a1bb3b7f1f0c6a
Kevin Ruscoe <hello@kevinruscoe.me>
2019-04-19 16:59:42 +0100

    fixes

--- src/woocommerce-after-add-to-cart.admin.php
+++ src/woocommerce-after-add-to-cart.admin.php
@@ -17,12 +17,11 @@ add_action(
 				exit;
 			}
 
-			$product = wc_get_product( $post_id );
-			$product->update_meta_data(
+			update_post_meta(
+				$post_id,
 				'_after_add_to_cart_redirection_id',
 				sanitize_text_field( wp_unslash( $_POST['_after_add_to_cart_redirection_id'] ) )
 			);
-			$product->save();
 		}
 	}
 );
@@ -90,8 +89,9 @@ add_action(
 			wp_die();
 		}
 
-		$term    = sanitize_text_field( wp_unslash( $_GET['term'] ) );
-		$exclude = absint( wp_unslash( $_GET['exclude'] ) );
+		$term         = sanitize_text_field( wp_unslash( $_GET['term'] ) );
+		$exclude      = absint( wp_unslash( $_GET['exclude'] ) );
+		$is_variation = 'product_variation' === get_product( $exclude )->post_type;
 
 		// @codingStandardsIgnoreLine
 		$results = $wpdb->get_results(
@@ -116,6 +116,10 @@ add_action(
 			'default_action' => $default_label,
 		];
 
+		if ( $is_variation ) {
+			$return['as_parent'] = 'Same as parent';
+		}
+
 		foreach ( $results as $result ) {
 			$return[ $result->id ] = sprintf(
 				"%s <span style='float: right; color: #000'>%s</span>",
@@ -127,5 +131,72 @@ add_action(
 		wp_die(
 			wp_json_encode( $return )
 		);
+	}
+);
+
+
+add_action(
+	'woocommerce_product_after_variable_attributes',
+	function ( $loop, $variation_data, $variation ) {
+		$options = [];
+
+		$current_value = get_post_meta( $variation->ID, '_after_add_to_cart_redirection_id', true );
+
+		if ( $current_value ) {
+			if ( 'default_action' === $current_value ) {
+				$default = get_option( 'woocommerce_cart_redirect_after_add' );
+
+				$default_label = "Default Action (don't redirect to cart)";
+				if ( 'yes' === $default ) {
+					$default_label = 'Default Action (redirect to cart)';
+				}
+
+				$options['default_action'] = $default_label;
+			} elseif ( 'as_parent' === $current_value ) {
+				$options['as_parent'] = 'Same as parent';
+			} else {
+				$options[ $current_value ] = sprintf(
+					"%s <span style='float: right; color: #000'>%s</span>",
+					get_the_title( $current_value ),
+					strtoupper( get_post_type( $current_value ) )
+				);
+			}
+		}
+
+		woocommerce_wp_select(
+			[
+				'name'              => '_after_add_to_cart_variantion_redirection_id[' . $variation->ID . ']',
+				'id'                => '_after_add_to_cart_variantion_redirection_id[' . $variation->ID . ']',
+				'class'             => 'wc-product-search',
+				'style'             => 'width: 80%;',
+				'label'             => 'Redirect To',
+				'desc_tip'          => true,
+				'description'       => 'Where should you be redirected to after adding this product to the cart?',
+				'custom_attributes' => [
+					'data-placeholder' => 'Select URL',
+					'data-action'      => 'after_add_to_cart_item_search',
+					'data-exclude'     => $variation->ID,
+				],
+				'options'           => $options,
+			]
+		);
+	},
+	10,
+	3
+);
+
+
+add_action(
+	'woocommerce_save_product_variation',
+	function ( $variation_id ) {
+		check_ajax_referer( 'save-variations', 'security' );
+
+		if ( isset( $_POST['_after_add_to_cart_variantion_redirection_id'][ $variation_id ] ) ) {
+			update_post_meta(
+				$variation_id,
+				'_after_add_to_cart_redirection_id',
+				sanitize_text_field( wp_unslash( $_POST['_after_add_to_cart_variantion_redirection_id'][ $variation_id ] ) )
+			);
+		}
 	}
 );
